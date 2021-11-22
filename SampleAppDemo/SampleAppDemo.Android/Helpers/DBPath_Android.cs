39fb264b844320671efc56126ca14e33b1fb7102
Virendra <virendra.thakur31@gmail.com>
2021-11-22 11:36:47 +0530

    DBPath_Android added.

---
+++ SampleAppDemo/SampleAppDemo.Android/Helpers/DBPath_Android.cs
@@ -0,0 +1,16 @@
+﻿using SampleAppDemo.Droid.Helpers;
+using SampleAppDemo.Helpers;
+using Xamarin.Forms;
+
+//[assembly: Dependency(typeof(DBPath_Android))]
+namespace SampleAppDemo.Droid.Helpers
+{
+    public static class DBPath_Android //: IDBPath
+    {
+        public static string GetDatabasePath(string databaseName)
+        {
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            return System.IO.Path.Combine(path, databaseName);
+        }
+    }
+}
