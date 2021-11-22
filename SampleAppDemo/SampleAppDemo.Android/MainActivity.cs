893d8b04df3f67d44e6d1af3ac02c31730619ddd
Virendra <virendra.thakur31@gmail.com>
2021-11-22 11:38:32 +0530

    MainActivity updated.

--- SampleAppDemo/SampleAppDemo.Android/MainActivity.cs
+++ SampleAppDemo/SampleAppDemo.Android/MainActivity.cs
@@ -4,10 +4,13 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using FreshMvvm;
+using SampleAppDemo.Services;
+using SampleAppDemo.Droid.Helpers;
 
 namespace SampleAppDemo.Droid
 {
-    [Activity(Label = "SampleAppDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SampleAppDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -16,6 +19,10 @@ namespace SampleAppDemo.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            var repository = new SQLiteService(DBPath_Android.GetDatabasePath("sample.db3"));
+            FreshIOC.Container.Register(repository);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
