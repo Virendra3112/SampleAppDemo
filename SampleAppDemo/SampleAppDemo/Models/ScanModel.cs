d290019fbb558bc8949ae39e34e30fdfd5a5dc02
Virendra <virendra.thakur31@gmail.com>
2021-11-22 11:34:30 +0530

    ScanModel added.

---
+++ SampleAppDemo/SampleAppDemo/Models/ScanModel.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace SampleAppDemo.Models
+{
+    public class ScanModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int? Id { get; set; }
+        public bool IsChecked { get; set; }
+        public string Name { get; set; }
+    }
+}
